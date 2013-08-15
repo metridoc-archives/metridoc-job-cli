75ece15b75448b1b56a5f6670137dd50e6dbdb23
Tommy Barker <mingus.karate@gmail.com>
2013-08-15 15:42:49 -0400

    cleanup code, more integ cli tests

---
+++ src/integTest/groovy/metridoc/cli/InstallSpec.groovy
@@ -0,0 +1,21 @@
+package metridoc.cli
+/**
+ * Created with IntelliJ IDEA on 8/15/13
+ * @author Tommy Barker
+ */
+class InstallSpec extends AbstractFunctionalSpec {
+
+    def bar = new File("${System.getProperty("user.home")}/.metridoc/jobs/metridoc-job-bar-0.1")
+
+    void "test install job"() {
+        when:
+        int exitCode = runCommand(["install", "src/test/testJobs/metridoc-job-bar-0.1.zip"], new File(baseWorkDir))
+
+        then:
+        0 == exitCode
+        bar.exists()
+
+        cleanup:
+        bar.deleteDir()
+    }
+}
