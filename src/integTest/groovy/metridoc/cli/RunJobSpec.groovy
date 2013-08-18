b69ce491f7be103d5ce958131f99ba92f57f0a79
Tommy Barker <thomas.p.barker@gmail.com>
2013-08-18 09:38:48 -0400

    test running a job after installation

--- src/integTest/groovy/metridoc/cli/RunJobSpec.groovy
+++ src/integTest/groovy/metridoc/cli/RunJobSpec.groovy
@@ -8,6 +8,14 @@ class RunJobSpec extends AbstractFunctionalSpec {
 
     def scriptLocation = "src/test/testJobs/script/simpleScript.groovy"
 
+    def setup() {
+        runCommand(["install", "src/test/testJobs/metridoc-job-bar-0.1.zip"])
+    }
+
+    def cleanup() {
+        assert new File("${System.getProperty("user.home")}/.metridoc/jobs/metridoc-job-bar-0.1").deleteDir()
+    }
+
     void "test running a script"() {
         when:
         int exitCode = runCommand([scriptLocation])
@@ -15,5 +23,14 @@ class RunJobSpec extends AbstractFunctionalSpec {
         then:
         0 == exitCode
         output.contains("simpleScript ran")
+    }
+
+    void "run a complex job"() {
+        when:
+        int exitCode = runCommand(["bar"])
+
+        then:
+        0 == exitCode
+        output.contains("bar has run")
     }
 }
