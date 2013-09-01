c19cf705bf2c170618863ba98e9a91a59ecaf457
Tommy Barker <thomas.p.barker@gmail.com>
2013-09-01 10:24:26 -0400

    change test job to guarantee grape works

--- src/test/testJobs/complexJob/metridoc-job-foo-0.1/src/main/groovy/foo.groovy
+++ src/test/testJobs/complexJob/metridoc-job-foo-0.1/src/main/groovy/foo.groovy
@@ -1,3 +1,5 @@
+@Grab(group='com.github.stefanbirkner', module='system-rules', version='1.3.1')
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog
 import org.springframework.core.io.ClassPathResource
 
 assert new ClassPathResource("fileInFoo.txt").exists()
