cacaf6980e3903795279985bb65d3403998e176c
Tommy Barker <thomas.p.barker@gmail.com>
2013-09-01 10:43:32 -0400

    when mdoc is up to date dont install

--- src/etc/install-mdoc.sh
+++ src/etc/install-mdoc.sh
@@ -2,14 +2,6 @@
 
 INSTALL_DIR="$HOME/.metridoc/cli/install"
 
-if [[ -d $INSTALL_DIR ]];
-then
-    echo "previous installation exists, deleting now"
-    rm -rf $INSTALL_DIR
-fi
-
-mkdir -p $INSTALL_DIR
-
 if [[ $1 ]];
 then
     echo "searching for version $1"
@@ -17,6 +9,20 @@ else
     MDOC_VERSION=`curl -s https://raw.github.com/metridoc/metridoc-job-cli/master/src/main/resources/MDOC_VERSION`
     echo "no version provided, installing latest version [$MDOC_VERSION]"
 fi
+
+if [[ -d $INSTALL_DIR ]];
+then
+    if grep -q $MDOC_VERSION $INSTALL_DIR/metridoc-job-cli/src/main/resources/MDOC_VERSION; then
+        echo "mdoc is up to date with version $MDOC_VERSION"
+        exit 0
+    fi
+    echo "previous installation exists, deleting now"
+    rm -rf $INSTALL_DIR
+fi
+
+mkdir -p $INSTALL_DIR
+
+
 
 GITHUB_URL="https://github.com/metridoc/metridoc-job-cli/archive/v$MDOC_VERSION.zip"
 SOURCE_FILE="metridoc-job-cli-$MDOC_VERSION.zip"
