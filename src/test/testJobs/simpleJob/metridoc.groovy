a7c6a0bcc87b08d4f434259b7dd63174dc0c081e
Tommy Barker <thomas.p.barker@gmail.com>
2013-09-08 09:23:12 -0400

    bump version of gorm lib, more tests

--- src/test/testJobs/simpleJob/metridoc.groovy
+++ src/test/testJobs/simpleJob/metridoc.groovy
@@ -1,6 +1,20 @@
 import foo.bar.Foo
+import grails.persistence.Entity
+import metridoc.core.MetridocScript
+import metridoc.tool.gorm.GormTool
 
 println "foo ran"
 new Foo()
 
+use(MetridocScript) {
+    includeTool(GormTool).enableGormFor(Bar)
+}
+
+Bar.list()
+
 return "foo ran"
+
+@Entity
+class Bar {
+    String foo
+}
