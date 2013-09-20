df6c14d0a6069b3f0a4258bf889539bee82ddb12
Tommy Barker <mingus.karate@gmail.com>
2013-09-20 17:00:30 -0400

    add info on global arg placement
    Issue #47

--- src/main/groovy/metridoc/cli/MetridocMain.groovy
+++ src/main/groovy/metridoc/cli/MetridocMain.groovy
@@ -392,7 +392,7 @@ class MetridocMain {
     protected List parseArgs() {
 
         def cli = new CliBuilder(
-                usage: "mdoc [<command> | <job> | help | help <job>] [job options]",
+                usage: "mdoc [global option] [<command> | <job> | help | help <job>] [job options]",
                 header: "\nGlobal Options:",
                 footer: "\nAvailable Commands:\n" +
                         " --> list-jobs                  lists all available jobs\n" +
